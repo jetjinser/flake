0ceb5f4f50ba57339e643efe4ae48aab8f3badd6
Jinser Kafka <aimer@purejs.icu>
2024-03-01 03:27:43 +0800

    fix: allow missing modules in makeModulesClosure

--- hosts/karenina/configuration.nix
+++ hosts/karenina/configuration.nix
@@ -7,11 +7,19 @@
     inputs.nixos-hardware.nixosModules.raspberry-pi-4
   ];
 
+  # https://github.com/NixOS/nixpkgs/issues/126755#issuecomment-869149243
+  nixpkgs.overlays = [
+    (final: super: {
+      makeModulesClosure = x:
+        super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
+
   system.stateVersion = "24.05";
 
-  # INFO: https://discourse.nixos.org/t/flake-to-create-a-simple-sd-image-for-rpi4-cross/35185/24
-  #       Disabling the whole `profiles/base.nix` module, which is responsible
-  #       for adding ZFS and a bunch of other unnecessary programs:
+  # https://discourse.nixos.org/t/flake-to-create-a-simple-sd-image-for-rpi4-cross/35185/24
+  # Disabling the whole `profiles/base.nix` module, which is responsible
+  # or adding ZFS and a bunch of other unnecessary programs:
   disabledModules = [
     "profiles/base.nix"
   ];
