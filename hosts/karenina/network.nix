914b4d414d6581efa00a3b26ad236876726fd7e4
Jinser Kafka <aimer@purejs.icu>
2024-03-01 19:37:34 +0800

    fix

--- hosts/karenina/network.nix
+++ hosts/karenina/network.nix
@@ -8,22 +8,24 @@ in
     networkmanager = {
       enable = true;
       ensureProfiles = {
-        dormitory-wifi = {
-          connection = {
-            id = "dormitory-wifi";
-            permissions = "";
-            type = "wifi";
-          };
-          ipv4.method = "auto";
-          ipv6.method = "auto";
-          wifi = {
-            ssid = SSID;
-            mode = "infrastructure";
-            password = SSIDpassword;
-          };
-          wifi-security = {
-            auth-alg = "open";
-            key-mgmt = "wpa-psk";
+        profiles = {
+          dormitory-wifi = {
+            connection = {
+              id = "dormitory-wifi";
+              permissions = "";
+              type = "wifi";
+            };
+            ipv4.method = "auto";
+            ipv6.method = "auto";
+            wifi = {
+              ssid = SSID;
+              mode = "infrastructure";
+              password = SSIDpassword;
+            };
+            wifi-security = {
+              auth-alg = "open";
+              key-mgmt = "wpa-psk";
+            };
           };
         };
       };
