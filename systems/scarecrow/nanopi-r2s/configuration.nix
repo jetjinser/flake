931926c462a2a31cee011afb1c2e538f497c0fa9
Jinser Kafka <aimer@purejs.icu>
2024-03-04 10:03:32 +0800

    may rk3288 can works

--- systems/scarecrow/nanopi-r2s/configuration.nix
+++ systems/scarecrow/nanopi-r2s/configuration.nix
@@ -9,19 +9,37 @@
     "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-new-kernel-no-zfs-installer.nix"
   ];
 
-  sdImage.compressImage = true;
-
   boot = {
-    kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_7;
+    kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
     loader = {
       grub.enable = false;
       generic-extlinux-compatible.enable = true;
     };
   };
 
-  hardware = {
-    enableRedistributableFirmware = true;
-    firmware = [ pkgs.wireless-regdb ];
+  nixpkgs.config.allowUnfree = true;
+  sdImage = {
+    compressImage = true;
+    imageBaseName = "nixos-nanopi-r2s-rk3328";
+    postBuildCommands = with pkgs; ''
+      dd if=${ubootRock64}/idbloader.img of=$img conv=fsync,notrunc bs=512 seek=64
+      dd if=${ubootRock64}/u-boot.itb of=$img conv=fsync,notrunc bs=512 seek=16384
+    '';
+  };
+
+  systemd.services."hardware-setup" = {
+    enable = true;
+    script = ''
+      # enable USB power (this is a hack compensating for the wrong device tree)
+      ${pkgs.libgpiod}/bin/gpioset 1 26=1
+      # initialize IR protocol handlers
+      ${pkgs.v4l-utils.override{withGUI = false;}}/bin/ir-keytable -p rc-5 -p rc-5-sz -p jvc -p sony -p nec -p sanyo -p mce_kbd -p rc-6 -p sharp -p xmp -p imon -p rc-mm
+    '';
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      RestartSec = 1;
+      Restart = "on-failure";
+    };
   };
 
   system.stateVersion = "24.05";
--- systems/scarecrow/nanopi-r2s/hardware.nix
+++ systems/scarecrow/nanopi-r2s/hardware.nix
@@ -3,66 +3,66 @@
 }:
 
 {
-  boot.loader = {
-    grub.enable = lib.mkDefault false;
-    # Enables the generation of /boot/extlinux/extlinux.conf.
-    generic-extlinux-compatible = {
-      enable = lib.mkDefault true;
-      useGenerationDeviceTree = true;
+  boot = {
+    loader = {
+      grub.enable = lib.mkDefault false;
+      # Enables the generation of /boot/extlinux/extlinux.conf.
+      generic-extlinux-compatible = {
+        enable = lib.mkDefault true;
+        useGenerationDeviceTree = true;
+      };
     };
+    kernelParams = [
+      "console=tty1"
+      "console=ttyS2,1500000n8"
+      "earlycon=uart8250,mmio32,0xfe660000"
+    ];
+    kernelPatches = [
+      {
+        name = "rockchip-config.patch";
+        patch = null;
+        extraConfig = ''
+          PCIE_ROCKCHIP_EP y
+          PCIE_ROCKCHIP_DW_HOST y
+          ROCKCHIP_VOP2 y
+        '';
+      }
+      {
+        name = "status-leds.patch";
+        patch = null;
+        extraConfig = ''
+          LED_TRIGGER_PHY y
+          USB_LED_TRIG y
+          LEDS_BRIGHTNESS_HW_CHANGED y
+          LEDS_TRIGGER_MTD y
+        '';
+      }
+    ];
+    initrd.availableKernelModules = [
+      ## Rockchip
+      ## Storage
+      "sdhci_of_dwcmshc"
+      "dw_mmc_rockchip"
+
+      "analogix_dp"
+      "io-domain"
+      "rockchip_saradc"
+      "rockchip_thermal"
+      "rockchipdrm"
+      "rockchip-rga"
+      "pcie_rockchip_host"
+      "phy-rockchip-pcie"
+      "phy_rockchip_snps_pcie3"
+      "phy_rockchip_naneng_combphy"
+      "phy_rockchip_inno_usb2"
+      "dwmac_rk"
+      "dw_wdt"
+      "dw_hdmi"
+      "dw_hdmi_cec"
+      "dw_hdmi_i2s_audio"
+      "dw_mipi_dsi"
+    ];
   };
-
-  boot.kernelParams = [
-    "console=tty1"
-    "console=ttyS2,1500000"
-    "earlycon=uart8250,mmio32,0xfe660000"
-  ];
-  boot.kernelPatches = [
-    {
-      name = "rockchip-config.patch";
-      patch = null;
-      extraConfig = ''
-        PCIE_ROCKCHIP_EP y
-        PCIE_ROCKCHIP_DW_HOST y
-        ROCKCHIP_VOP2 y
-      '';
-    }
-    {
-      name = "status-leds.patch";
-      patch = null;
-      extraConfig = ''
-        LED_TRIGGER_PHY y
-        USB_LED_TRIG y
-        LEDS_BRIGHTNESS_HW_CHANGED y
-        LEDS_TRIGGER_MTD y
-      '';
-    }
-  ];
-
-  boot.initrd.availableKernelModules = [
-    ## Rockchip
-    ## Storage
-    "sdhci_of_dwcmshc"
-    "dw_mmc_rockchip"
-
-    "analogix_dp"
-    "io-domain"
-    "rockchip_saradc"
-    "rockchip_thermal"
-    "rockchipdrm"
-    "rockchip-rga"
-    "pcie_rockchip_host"
-    "phy-rockchip-pcie"
-    "phy_rockchip_snps_pcie3"
-    "phy_rockchip_naneng_combphy"
-    "phy_rockchip_inno_usb2"
-    "dwmac_rk"
-    "dw_wdt"
-    "dw_hdmi"
-    "dw_hdmi_cec"
-    "dw_hdmi_i2s_audio"
-    "dw_mipi_dsi"
-  ];
 
   # Most Rockchip CPUs (especially with hybrid cores) work best with "schedutil"
   powerManagement.cpuFreqGovernor = "schedutil";
