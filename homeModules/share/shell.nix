9d6179046a3b24080b5149589b77e1289dc1077f
Jinser Kafka <aimer@purejs.icu>
2024-03-05 10:14:20 +0800

    auto gen fish function

--- homeModules/share/shell.nix
+++ homeModules/share/shell.nix
@@ -7,27 +7,30 @@ in
   programs = {
     fish = {
       enable = true;
-      shellAliases = {
+      interactiveShellInit = builtins.readFile (config_path + /fish/config.fish);
+
+      shellAbbrs = {
         g = "git";
         cdtmp = "cd (mktemp -d /tmp/jinser-XXXXXX)";
         decolorize = ''
           sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,3})*)?[mGK]//g"
         '';
         nf = "nix flake";
+        eproxy = "set -e {HTTP, HTTPS, ALL, FTP, RSYNC}_PROXY";
       };
+
       functions =
         let
+          fnExt = ".fish";
           fn = name: builtins.readFile (config_path + /fish/functions/${name}.fish);
+          allFns = with lib;
+            mapAttrsToList
+              (n: _: removeSuffix fnExt n)
+              (filterAttrs
+                (n: v: v == "regular" || hasSuffix fnExt n)
+                (builtins.readDir (config_path + /fish/functions)));
         in
-        lib.genAttrs
-          [
-            "hst"
-            "del"
-            "cht"
-            "nr"
-          ]
-          fn;
-      interactiveShellInit = builtins.readFile (config_path + /fish/config.fish);
+        lib.genAttrs allFns fn;
     };
 
     starship.enable = true;
