a2cfafd54c948611acf4ba2ba9c00ab3a441057a
Jinser Kafka <aimer@purejs.icu>
2024-06-15 01:21:57 +0800

    flakeModules: remove rust devshell

--- flakeModules/devshell/default.nix
+++ flakeModules/devshell/default.nix
@@ -1,12 +1,42 @@
-{ inputs
+{ lib
+, inputs
+, config
 , ...
 }:
 
 {
   imports = [
     inputs.devshell.flakeModule
-
-    ./console
-    ./rust
   ];
+
+  perSystem = { pkgs, ... }:
+    {
+      devshells.default =
+        let
+          inherit (pkgs.stdenv) isLinux isDarwin;
+          ab = attr: lib.mkMerge [
+            share.${attr}
+            remote.${attr}
+
+            (lib.mkIf isLinux nixos.${attr})
+            (lib.mkIf isDarwin darwin.${attr})
+          ];
+
+          share = import ./share.nix { inherit lib pkgs inputs config; };
+          darwin = import ./darwin.nix { inherit lib pkgs config; };
+          nixos = import ./nixos.nix { inherit lib pkgs; };
+          remote = import ./remote.nix;
+        in
+        {
+          motd = ''
+            {italic}{99}🦾 Life in Nix 👾{reset}
+            $(type -p menu &>/dev/null && menu)
+          '';
+          name = "NixConsole";
+
+          env = ab "env";
+          commands = ab "commands";
+          packages = ab "packages";
+        };
+    };
 }
--- flakeModules/devshell/rust/default.nix
+++
@@ -1,6 +0,0 @@
-{
-  imports = [
-    ./stable.nix
-    ./nightly.nix
-  ];
-}
