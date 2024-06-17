d2f4b0192deebbb3276df7abfe127c43b92cdda6
Jinser Kafka <aimer@purejs.icu>
2024-06-18 01:35:06 +0800

    dorothy(home): shell abbr cdf & cdw

--- hosts/dorothy/home/terminal.nix
+++ hosts/dorothy/home/terminal.nix
@@ -2,4 +2,9 @@
   home.sessionPath = [
     "$HOME/.config/guix/current/bin"
   ];
+
+  programs.fish.shellAbbrs = {
+    cdf = "cd ~/vie/projet/flake";
+    cdw = "cd ~/vie/writing/forest";
+  };
 }
