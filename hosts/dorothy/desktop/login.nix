18ed071a629d78e43191e9329eeb6ae8402f063f
Jinser Kafka <aimer@purejs.icu>
2024-09-06 22:51:16 +0800

    update

--- hosts/dorothy/desktop/login.nix
+++ hosts/dorothy/desktop/login.nix
@@ -3,15 +3,7 @@
 , ...
 }:
 
-let
-  inherit (flake.inputs) nixpkgs-uwsm;
-in
 {
-  imports = [
-    # "${nixpkgs-uwsm}/nixos/modules/services/misc/graphical-desktop.nix"
-    "${nixpkgs-uwsm}/nixos/modules/programs/wayland/uwsm.nix"
-  ];
-
   services.xserver = {
     enable = true;
     excludePackages = [
@@ -20,17 +12,6 @@ in
     displayManager.gdm = {
       enable = true;
       wayland = true;
-    };
-  };
-
-  programs.uwsm = {
-    enable = true;
-    waylandCompositors = {
-      niri = {
-        prettyName = "Niri";
-        comment = "Niri compositor managed by UWSM";
-        binPath = "/run/current-system/sw/bin/niri";
-      };
     };
   };
 
--- hosts/dorothy/ime.nix
+++ hosts/dorothy/ime.nix
@@ -7,6 +7,7 @@
     enable = true;
     type = "fcitx5";
     fcitx5 = {
+      waylandFrontend = true;
       addons = with pkgs; [
         # fcitx5-rime
         fcitx5-chinese-addons
