bca5e3fd7b92812a22cf6537bb217e0b99235528
Jinser Kafka <aimer@purejs.icu>
2024-09-11 01:18:42 +0800

    update ssh-keys

--- symbols/users/config.nix
+++ symbols/users/config.nix
@@ -7,6 +7,7 @@
       sshKeys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIILkVlWmF+kMCPIdWkvDsXFHDtq84njf8NVN7GxUCAHs julien@darwin"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF0sQOE/u9FfEp45LtN1AEx/8vuHxB0BiRb4Asy2o+Yb jinser@bendemann"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIO9OWTLHeJinGsCD8JX4OBK7IwGh+st1sQC/2YHCQ5m jinser@dorothy"
       ];
     };
   };
