f95a98034ae4b29d745e7009c2fbc79b32500f7b
Jinser Kafka <aimer@purejs.icu>
2024-09-13 08:09:36 +0800

    fix(service, cosimo): add privKey

--- hosts/cosimo/services/collab.nix
+++ hosts/cosimo/services/collab.nix
@@ -4,11 +4,13 @@
 
 let
   inherit (config.users) users;
+  inherit (config.sops) secrets;
 in
 {
   services.radicle = {
     enable = true;
     publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAaMWa9Od5wh1+ozsjKg4KJvh1jR/UBXgHY1DQLGTfqA radicle";
+    privateKeyFile = secrets.radPriKey.path;
     httpd = {
       enable = true;
       listenPort = 8009;
