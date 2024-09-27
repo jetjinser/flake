50e395747665a9af52b85cbb85bc6ebbeb9b94b1
Jinser Kafka <aimer@purejs.icu>
2024-09-27 09:06:35 +0800

    add java-dev template

--- templates/default.nix
+++ templates/default.nix
@@ -6,18 +6,23 @@ rec {
     description = "A startup basic devshell project";
   };
 
-  rust-dev = {
-    path = ./rust-dev;
-    description = "A startup rust project with devshell";
+  hs-dev = {
+    path = ./hs-dev;
+    description = "A startup Haskell project with devshell";
+  };
+
+  java-dev = {
+    path = ./java-dev;
+    description = "A startup Java and Gradle project with devshell";
   };
 
   python-dev = {
     path = ./python-dev;
-    description = "A startup python project with devshell";
+    description = "A startup Python project with devshell";
   };
 
-  hs-dev = {
-    path = ./hs-dev;
-    description = "A startup haskell project with devshell";
+  rust-dev = {
+    path = ./rust-dev;
+    description = "A startup Rust project with devshell";
   };
 }
---
+++ templates/java-dev/nix/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A startup basic project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+
+      perSystem = { pkgs, ... }: {
+        devshells.default = {
+          packages = with pkgs; [
+            jdk_headless
+            kotlin
+
+            gradle
+
+            kotlin-language-server
+
+            vscode-langservers-extracted
+          ];
+        };
+      };
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+    };
+}
