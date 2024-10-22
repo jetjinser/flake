f3ca0cdabdbc23535d5e421e11fff734dd71f6a2
Jinser Kafka <aimer@purejs.icu>
2024-10-23 01:58:31 +0800

    config(dorothy): use greetd(guigreet) instead of gdm

--- hosts/dorothy/desktop/login.nix
+++ hosts/dorothy/desktop/login.nix
@@ -1,28 +1,17 @@
 { pkgs
+, lib
 , ...
 }:
 
 {
-  services.xserver = {
+  services.greetd = {
     enable = true;
-    excludePackages = [
-      pkgs.xterm
-    ];
-    displayManager.gdm = {
-      enable = true;
-      wayland = true;
+    settings = {
+      default_session.command = ''
+        ${lib.getExe pkgs.greetd.tuigreet} --cmd niri-session --remember --greeting 'welcome back'
+      '';
     };
   };
-
-  # config gdm face workaround
-  # https://discourse.nixos.org/t/setting-the-user-profile-image-under-gnome/36233/5
-  # https://help.gnome.org/admin/gdm/stable/overview.html.en#facebrowser
-  system.activationScripts.script.text = ''
-    mkdir -p /var/lib/AccountsService/{icons,users}
-    cp ${../../../assets/face.jpg} /var/lib/AccountsService/icons/jinser
-    echo -e "[User]\nIcon=/var/lib/AccountsService/icons/jinser\n" > /var/lib/AccountsService/users/jinser
-  '';
-
 
   services.xserver.desktopManager.runXdgAutostartIfNone = true;
 }
