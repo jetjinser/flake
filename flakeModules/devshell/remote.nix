fd92e3db40c2b6db37272b8913d9a26703ca1122
Jinser Kafka <aimer@purejs.icu>
2024-10-24 23:40:33 +0800

    devshell: use deploy-rs pkg

--- flakeModules/devshell/remote.nix
+++ flakeModules/devshell/remote.nix
@@ -7,7 +7,6 @@
     let
       inherit (config.malib pkgs) mkCmdGroup;
       RemoteCmdGroup = mkCmdGroup "Remote" [
-
         {
           name = "anywhere";
           help = "Use nixos-anywhere to remotely install NixOS to the specified host";
@@ -15,16 +14,17 @@
             nix run github:nix-community/nixos-anywhere -- --build-on-remote --flake .#$1 ''${@:2}
           '';
         }
-        {
-          name = "deploy";
-          help = "run deploy-rs";
-          command = "nix run github:serokell/deploy-rs -- $@";
-        }
+        # {
+        #   name = "deploy";
+        #   help = "run deploy-rs";
+        #   command = "nix run github:serokell/deploy-rs -- $@";
+        # }
       ];
     in
     {
       devshells.default = {
         commands = RemoteCmdGroup;
+        packages = [ pkgs.deploy-rs ];
       };
     };
 }
