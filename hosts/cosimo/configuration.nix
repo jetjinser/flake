80327a5b91ce8a5d6d6a701fa66e593a12e64632
Jinser Kafka <aimer@purejs.icu>
2024-11-14 00:10:40 +0800

    loader(cosimo): disable efiInstallAsRemovable

--- hosts/cosimo/configuration.nix
+++ hosts/cosimo/configuration.nix
@@ -29,7 +29,6 @@
         # no need to set devices, disko will add all devices that have a EF02 partition to the list already
         # devices = [ ];
         efiSupport = true;
-        efiInstallAsRemovable = true;
       };
     };
   };
