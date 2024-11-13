a5e1a50ec19cba20ba4caae3c34741b60b1fcf52
Jinser Kafka <aimer@purejs.icu>
2024-11-14 00:36:42 +0800

    loader(cosimo): enable efiTouchable

--- hosts/cosimo/configuration.nix
+++ hosts/cosimo/configuration.nix
@@ -25,6 +25,10 @@
     kernelModules = [ ];
     extraModulePackages = [ ];
     loader = {
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
       grub = {
         # no need to set devices, disko will add all devices that have a EF02 partition to the list already
         # devices = [ ];
