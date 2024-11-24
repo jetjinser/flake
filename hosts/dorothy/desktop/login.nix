948550c9914d439ff0d9ff7478d54dbf2fbcf8e8
Jinser Kafka <aimer@purejs.icu>
2024-11-25 02:48:49 +0800

    service(dorothy): turnoff keyboard backlight on boot

--- hosts/dorothy/desktop/login.nix
+++ hosts/dorothy/desktop/login.nix
@@ -14,4 +14,15 @@
   };
 
   services.xserver.desktopManager.runXdgAutostartIfNone = true;
+
+  systemd.services.turnoffKbdBacklight = {
+    description = "Turns off keyboard backlight when system on.";
+    wantedBy = [ "multi-user.target" ];
+
+    script =
+      let brightnessctlBin = lib.getExe' pkgs.brightnessctl "brightnessctl";
+      in "${brightnessctlBin} -d 'platform::kbd_backlight' set 0";
+
+    serviceConfig.Type = "oneshot";
+  };
 }
