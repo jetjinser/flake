e61af14a5cbc80617da4332f52d6dc41207686b3
Jinser Kafka <aimer@purejs.icu>
2024-11-25 10:49:13 +0800

    proxy(dorothy): enable tun sing-box

--- hosts/dorothy/proxy.nix
+++ hosts/dorothy/proxy.nix
@@ -10,17 +10,28 @@ let
     _secret = secrets.${k}.path;
   };
   secretGenerator = with lib; flip genAttrs mkSecret;
+
+  nameservers = {
+    AliDNS = "223.5.5.5";
+    CloudFlare = "1.1.1.1";
+  };
 in
 {
   # NOTE: global, since SwitchyOmega does not work on my Firefox
   # networking.proxy.default = "http://127.0.0.1:7890/";
 
-  systemd.services.sing-box.wantedBy = lib.mkForce [ ];
+  networking.firewall.trustedInterfaces = [ "tun0" ];
+  boot.kernel.sysctl = {
+    "net.ipv4.conf.all.forwarding" = true;
+    "net.ipv6.conf.all.forwarding" = true;
+  };
+
   sops.secrets = {
     server = { };
     password = { };
     method = { };
   };
+  systemd.services.sing-box.wantedBy = lib.mkForce [ ];
   services.sing-box =
     let
       proxy = lib.mergeAttrsList [
@@ -51,10 +62,23 @@ in
       settings = {
         log.level = "warn";
         inbounds = [
+          # {
+          #   type = "mixed";
+          #   listen = "::";
+          #   listen_port = 7890;
+          # }
           {
-            type = "mixed";
-            listen = "::";
-            listen_port = 7890;
+            type = "tun";
+            tag = "tun-in";
+            interface_name = "tun0";
+            address = [
+              "172.19.0.1/30"
+              "fd00::1/126"
+            ];
+            auto_route = true;
+            strict_route = true;
+            stack = "system";
+            sniff = true;
           }
         ];
         outbounds = [
@@ -67,24 +91,56 @@ in
             tag = "block";
             type = "block";
           }
+          {
+            type = "dns";
+            tag = "dns-out";
+          }
         ];
-        route = {
-          final = "direct";
+        dns = {
+          servers = [
+            {
+              tag = "AliDNS";
+              address = "https://${nameservers.AliDNS}/dns-query";
+              address_strategy = "prefer_ipv4";
+              detour = "direct";
+            }
+            {
+              tag = "CloudFlare";
+              address = "https://${nameservers.CloudFlare}/dns-query";
+              detour = "direct";
+            }
+            {
+              tag = "block";
+              address = "rcode://success";
+            }
+          ];
           rules = [
             {
-              outbound = "direct";
-              geosite = [ "private" ];
+              geosite = [ "cn" ];
+              server = "AliDNS";
+              disable_cache = false;
             }
+            {
+              geosite = [ "category-ads-all" ];
+              server = "block";
+              disable_cache = true;
+            }
+          ];
+          final = "CloudFlare";
+        };
+        route = {
+          auto_detect_interface = true;
+          final = "direct";
+          rules = [
+            { outbound = "dns-out"; protocol = "dns"; }
+            { outbound = "direct"; geosite = [ "private" ]; }
             {
               outbound = "proxy";
               type = "logical";
               mode = "or";
               rules = [
                 { geosite = [ "geolocation-!cn" ]; }
-                {
-                  geoip = [ "cn" ];
-                  invert = true;
-                }
+                { geoip = [ "cn" ]; invert = true; }
               ];
             }
             {
