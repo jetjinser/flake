e19aeba1380a8f223612c93c9b52cd48dcdd6865
Jinser Kafka <aimer@purejs.icu>
2024-11-29 10:42:43 +0800

    proxy(dorothy): please working

--- hosts/dorothy/proxy.nix
+++ hosts/dorothy/proxy.nix
@@ -10,16 +10,8 @@ let
     _secret = secrets.${k}.path;
   };
   secretGenerator = with lib; flip genAttrs mkSecret;
-
-  nameservers = {
-    AliDNS = "223.5.5.5";
-    CloudFlare = "1.1.1.1";
-  };
 in
 {
-  # NOTE: global, since SwitchyOmega does not work on my Firefox
-  # networking.proxy.default = "http://127.0.0.1:7890/";
-
   networking.firewall.trustedInterfaces = [ "tun0" ];
   boot.kernel.sysctl = {
     "net.ipv4.conf.all.forwarding" = true;
@@ -31,22 +23,11 @@ in
     password = { };
     method = { };
   };
-  systemd.services.sing-box.wantedBy = lib.mkForce [ ];
   services.sing-box =
     let
       proxy = lib.mergeAttrsList [
-        {
-          type = "shadowsocks";
-          tag = "proxy";
-          # server_port = 28018;
-          server_port = 49148;
-        }
-        (secretGenerator
-          [
-            "server"
-            "password"
-            "method"
-          ])
+        { type = "shadowsocks"; tag = "proxy"; server_port = 49148; }
+        (secretGenerator [ "server" "password" "method" ])
         {
           multiplex = {
             enabled = true;
@@ -60,93 +41,69 @@ in
     {
       enable = true;
       settings = {
-        log.level = "warn";
+        log.level = "info";
         inbounds = [
-          # {
-          #   type = "mixed";
-          #   listen = "::";
-          #   listen_port = 7890;
-          # }
           {
             type = "tun";
             tag = "tun-in";
             interface_name = "tun0";
-            address = [
-              "172.19.0.1/30"
-              "fd00::1/126"
-            ];
+            address = [ "172.19.0.1/30" "fd00::1/126" ];
             auto_route = true;
             strict_route = true;
-            stack = "system";
+            stack = "mixed";
             sniff = true;
           }
         ];
         outbounds = [
           proxy
-          {
-            tag = "direct";
-            type = "direct";
-          }
-          {
-            tag = "block";
-            type = "block";
-          }
-          {
-            type = "dns";
-            tag = "dns-out";
-          }
+          { tag = "direct"; type = "direct"; }
+          { tag = "block"; type = "block"; }
+          { type = "dns"; tag = "dns-out"; }
         ];
         dns = {
           servers = [
             {
-              tag = "AliDNS";
-              address = "https://${nameservers.AliDNS}/dns-query";
-              address_strategy = "prefer_ipv4";
+              tag = "dns_proxy";
+              address = "https://1.1.1.1/dns-query";
               detour = "direct";
             }
             {
-              tag = "CloudFlare";
-              address = "https://${nameservers.CloudFlare}/dns-query";
+              tag = "dns_direct";
+              address = "https://223.5.5.5/dns-query";
               detour = "direct";
             }
-            {
-              tag = "block";
-              address = "rcode://success";
-            }
+            { tag = "dns_block"; address = "rcode://success"; }
           ];
           rules = [
-            {
-              geosite = [ "cn" ];
-              server = "AliDNS";
-              disable_cache = false;
-            }
+            { outbound = [ "any" ]; server = "dns_direct"; }
+            { geosite = [ "geolocation-!cn" ]; server = "dns_proxy"; }
             {
               geosite = [ "category-ads-all" ];
-              server = "block";
+              server = "dns_block";
               disable_cache = true;
             }
           ];
-          final = "CloudFlare";
+          final = "dns_direct";
+          independent_cache = true;
+        };
+        ntp = {
+          enabled = true;
+          server = "cn.ntp.org.cn";
+          server_port = 123;
+          interval = "30m";
+          detour = "direct";
         };
         route = {
           auto_detect_interface = true;
-          final = "direct";
+          final = "proxy";
           rules = [
             { outbound = "dns-out"; protocol = "dns"; }
             { outbound = "direct"; geosite = [ "private" ]; }
-            {
-              outbound = "proxy";
-              type = "logical";
-              mode = "or";
-              rules = [
-                { geosite = [ "geolocation-!cn" ]; }
-                { geoip = [ "cn" ]; invert = true; }
-              ];
-            }
+            { outbound = "block"; geosite = [ "category-ads-all" ]; }
             {
               outbound = "direct";
               type = "logical";
-              mode = "and";
+              mode = "or";
               rules = [
                 { geosite = [ "cn" ]; }
                 { geoip = [ "cn" ]; }
@@ -156,6 +113,4 @@ in
         };
       };
     };
-
-  systemd.services.sing-box.serviceConfig.UMask = "0077";
 }
