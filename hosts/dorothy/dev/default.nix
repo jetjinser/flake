8ec813d084115436c4f02c990380223ae35ee68e
Jinser Kafka <aimer@purejs.icu>
2024-12-07 14:54:14 +0800

    config(dorothy): split sops

--- hosts/dorothy/dev/default.nix
+++ hosts/dorothy/dev/default.nix
@@ -3,4 +3,26 @@
     # ./container.nix
     # ./virt.nix
   ];
+
+  # TODO: get help with preservation?
+  #
+  # sops = {
+  #   secrets.waka_api_key = { };
+  #   templates."wakatime.cfg".content = ''
+  #     [settings]
+  #     debug = false
+  #     hidefilenames = false
+  #     ignore =
+  #         COMMIT_EDITMSG$
+  #         PULLREQ_EDITMSG$
+  #         MERGE_MSG$
+  #         TAG_EDITMSG$
+  #     api_url = https://waka.yeufossa.org/api
+  #     api_key = "${config.sops.placeholder.waka_api_key}"
+  #   '';
+  # };
+  #
+  # systemd.tmpfiles.settings.generated = {
+  #   "/home/${myself}/.wakatime.cfg".f = { user = myself; group = "users"; mode = "0400"; };
+  # };
 }
--- hosts/dorothy/sops.nix
+++ hosts/dorothy/sops.nix
@@ -16,10 +16,8 @@ in
 
   sops = {
     defaultSopsFile = ./secrets.yaml;
-    age.keyFile = "/persist/home/${myself}/.config/sops/age/keys.txt";
+    age.keyFile = "/home/${myself}/.config/sops/age/keys.txt";
     secrets = {
-      waka_api_key = { };
-
       # spotify_username = {
       #   owner = users.spotifyd.name;
       # };
@@ -27,17 +25,5 @@ in
       #   owner = users.spotifyd.name;
       # };
     };
-    templates."wakatime.cfg".content = ''
-      [settings]
-      debug = false
-      hidefilenames = false
-      ignore =
-          COMMIT_EDITMSG$
-          PULLREQ_EDITMSG$
-          MERGE_MSG$
-          TAG_EDITMSG$
-      api_url = https://waka.yeufossa.org/api
-      api_key = "${config.sops.placeholder.waka_api_key}"
-    '';
   };
 }
