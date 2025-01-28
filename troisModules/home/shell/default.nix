f7a3aed1d63a5cfdd6d8576ccce3f329f512d2ee
Jinser Kafka <aimer@purejs.icu>
2025-01-29 00:57:14 +0800

    starship: use moonbit-patched package

--- troisModules/home/shell/default.nix
+++ troisModules/home/shell/default.nix
@@ -1,3 +1,8 @@
+{ pkgs
+, lib
+, ...
+}:
+
 let
   config_path = ../../../config;
 in
@@ -14,6 +19,20 @@ in
 
     starship = {
       enable = true;
+      package = pkgs.starship.overrideAttrs (old: rec {
+        version = "moonbit-lang";
+        src = pkgs.fetchFromGitHub {
+          owner = "jetjinser";
+          repo = "starship";
+          rev = "refs/heads/${version}";
+          hash = "sha256-aeRlnkgu2IOW8Xv+gaby4SQFttxeZdn3LxWs5uuyOpE=";
+        };
+        cargoDeps = old.cargoDeps.overrideAttrs (lib.const {
+          name = "${old.pname}-vendor.tar.gz";
+          inherit src;
+          outputHash = "sha256-6M9LCyp0amZ/pJySeMe75sP/IaA6Tta6djVTmtcxQTc=";
+        });
+      });
       enableFishIntegration = true;
       settings = builtins.fromTOML (builtins.readFile (config_path + /starship.toml));
     };
