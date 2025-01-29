bbdece9f4f08a7c64a6df6e23a3b7f667a5b2683
Jinser Kafka <aimer@purejs.icu>
2025-01-29 14:11:44 +0800

    networking(sheep): update ns

--- hosts/sheep/network.nix
+++ hosts/sheep/network.nix
@@ -15,11 +15,11 @@ in
     interfaces = {
       ens18.ipv4.addresses = [{
         address = "192.168.114.72";
-        prefixLength = 21;
+        prefixLength = 24;
       }];
     };
     defaultGateway = {
-      address = "192.168.113.254";
+      address = "192.168.114.254";
       interface = "ens18";
     };
 
