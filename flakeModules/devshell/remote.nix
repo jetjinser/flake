1e74728af7f73aab9e6969e97bef8d0b29ef8d7d
Jinser Kafka <aimer@purejs.icu>
2025-02-14 09:39:48 +0800

    devshell: anywhere use latest build-on-remote arg

--- flakeModules/devshell/remote.nix
+++ flakeModules/devshell/remote.nix
@@ -13,7 +13,7 @@
           name = "anywhere";
           help = "Use nixos-anywhere to remotely install NixOS to the specified host";
           command = ''
-            nix run github:nix-community/nixos-anywhere -- --build-on-remote --flake .#$1 ''${@:2}
+            nix run github:nix-community/nixos-anywhere -- --build-on remote --flake .#$1 ''${@:2}
           '';
         }
         # {
