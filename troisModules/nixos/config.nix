d3b06b0e4d68ed3858f63d4a89f78006aff6c5b6
Jinser Kafka <aimer@purejs.icu>
2025-02-25 00:03:13 +0800

    config(deploy-rs): apply overlays

--- troisModules/nixos/config.nix
+++ troisModules/nixos/config.nix
@@ -1,8 +1,24 @@
 {
+  flake,
+  pkgs,
+  ...
+}:
+
+{
   # i18n.defaultLocale = "zh_CN.UTF-8";
 
   # from whonix
   environment.etc.machine-id.source = ../machine-id;
 
   programs.command-not-found.enable = false;
+
+  nixpkgs.overlays = [
+    flake.inputs.deploy-rs.overlays.default
+    (self: super: {
+      deploy-rs = {
+        inherit (pkgs) deploy-rs;
+        lib = super.deploy-rs.lib;
+      };
+    })
+  ];
 }
