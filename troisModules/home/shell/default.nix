1f049ed33d666b61d9f2b84c6cff27da202fa8ab
Jinser Kafka <aimer@purejs.icu>
2025-03-11 01:00:34 +0800

    fish: default theme

--- troisModules/home/shell/default.nix
+++ troisModules/home/shell/default.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  lib,
   ...
 }:
 
@@ -12,12 +11,6 @@ let
     rev = "81e2ebcdc2ed815d17db962055645ccf2125560c";
     hash = "sha256-ePs7UlgQqh3ptRXUNlY/BDa/1aH9q3dGa3h0or/e6Kk=";
   };
-  rose-pine = pkgs.fetchFromGitHub {
-    owner = "rose-pine";
-    repo = "fish";
-    rev = "38aab5baabefea1bc7e560ba3fbdb53cb91a6186";
-    hash = "sha256-bSGGksL/jBNqVV0cHZ8eJ03/8j3HfD9HXpDa8G/Cmi8=";
-  };
 in
 {
   programs = {
@@ -25,7 +18,6 @@ in
       enable = true;
       interactiveShellInit = ''
         # source ${./tide.fish}
-        fish_config theme choose "Rosé Pine"
         source ${LS_COLORS}/lscolors.csh
       '';
       preferAbbrs = true;
@@ -40,9 +32,5 @@ in
 
     nix-index.enable = true;
     nix-index-database.comma.enable = true;
-  };
-  xdg.configFile."fish/themes" = {
-    source = "${rose-pine}/themes";
-    recursive = true;
   };
 }
