2ff95980fcb96ac8b82b06c822318e0cd2667058
Jinser Kafka <aimer@purejs.icu>
2025-04-07 00:13:38 +0800

    chore

--- troisModules/nixos/nix.nix
+++ troisModules/nixos/nix.nix
@@ -31,7 +31,7 @@ in
       experimental-features = "nix-command flakes";
       substituters = [
         # "https://mirrors.cernet.edu.cn/nix-channels/store"
-        "https://mirrors.ustc.edu.cn/nix-channels/store"
+        "https://mirrors.ustc.edu.cn/nix-channels/store/"
         # down: Fri Feb 14 12:47:20 AM CST 2025
         # "https://mirror.sjtu.edu.cn/nix-channels/store"
       ];
