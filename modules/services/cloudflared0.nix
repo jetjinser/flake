275cc23dcf9e4260604e03695d463dcb908d5199
Jinser Kafka <aimer@purejs.icu>
2025-05-02 18:40:10 +0800

    module(cloudflared0): special `~` in auto route

--- modules/services/cloudflared0.nix
+++ modules/services/cloudflared0.nix
@@ -46,65 +46,65 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    users.users = lib.mkIf (cfg.user == defaultUser) {
-      ${defaultUser} = {
-        inherit (cfg) group;
-        isSystemUser = true;
-      };
-    };
-    users.groups = lib.mkIf (cfg.group == defaultUser) {
-      ${defaultUser} = { };
-    };
-
-    services.cloudflared =
-      let
-        serveIng =
-          domain: subdomain: port:
-          let
-            prefix = if (subdomain == "~") then "" else "${subdomain}.";
-          in
-          {
-            "${prefix}${domain}" = "http://localhost:${toString port}";
-          };
-        serveIng' = serveIng cfg.domain;
-      in
-      {
-        inherit (cfg) enable;
-        tunnels = {
-          ${cfg.tunnelID} = {
-            inherit (cfg) credentialsFile certificateFile;
-            default = "http_status:404";
-            ingress = lib.concatMapAttrs serveIng' cfg.ingress;
-          };
+  config = lib.mkIf cfg.enable (
+    let
+      mkPrefix = x: if (x == "~") then "" else "${x}.";
+    in
+    {
+      users.users = lib.mkIf (cfg.user == defaultUser) {
+        ${defaultUser} = {
+          inherit (cfg) group;
+          isSystemUser = true;
         };
       };
-    systemd.services."cloudflared-tunnel-dns-setup-${cfg.tunnelID}" = {
-      description = "DNS Setup for Cloudflared Tunnel";
-      after = [
-        "network.target"
-        "network-online.target"
-      ];
-      wants = [
-        "network.target"
-        "network-online.target"
-      ];
-      before = [ "cloudflared-tunnel-${cfg.tunnelID}.service" ];
-      wantedBy = [ "multi-user.target" ];
-
-      environment.TUNNEL_ORIGIN_CERT = cfg.certificateFile;
-
-      script = lib.concatMapStringsSep "\n" (subdomain: ''
-        ${config.services.cloudflared.package}/bin/cloudflared tunnel route dns ${cfg.tunnelID} ${subdomain}.${cfg.domain}
-      '') (builtins.attrNames cfg.ingress);
-
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = "yes";
-        DynamicUser = true;
-        User = cfg.user;
-        Group = cfg.group;
+      users.groups = lib.mkIf (cfg.group == defaultUser) {
+        ${defaultUser} = { };
       };
-    };
-  };
+
+      services.cloudflared =
+        let
+          serveIng = domain: subdomain: port: {
+            "${mkPrefix subdomain}${domain}" = "http://localhost:${toString port}";
+          };
+          serveIng' = serveIng cfg.domain;
+        in
+        {
+          inherit (cfg) enable;
+          tunnels = {
+            ${cfg.tunnelID} = {
+              inherit (cfg) credentialsFile certificateFile;
+              default = "http_status:404";
+              ingress = lib.concatMapAttrs serveIng' cfg.ingress;
+            };
+          };
+        };
+      systemd.services."cloudflared-tunnel-dns-setup-${cfg.tunnelID}" = {
+        description = "DNS Setup for Cloudflared Tunnel";
+        after = [
+          "network.target"
+          "network-online.target"
+        ];
+        wants = [
+          "network.target"
+          "network-online.target"
+        ];
+        before = [ "cloudflared-tunnel-${cfg.tunnelID}.service" ];
+        wantedBy = [ "multi-user.target" ];
+
+        environment.TUNNEL_ORIGIN_CERT = cfg.certificateFile;
+
+        script = lib.concatMapStringsSep "\n" (subdomain: ''
+          ${config.services.cloudflared.package}/bin/cloudflared tunnel route dns ${cfg.tunnelID} ${mkPrefix subdomain}${cfg.domain}
+        '') (builtins.attrNames cfg.ingress);
+
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = "yes";
+          DynamicUser = true;
+          User = cfg.user;
+          Group = cfg.group;
+        };
+      };
+    }
+  );
 }
