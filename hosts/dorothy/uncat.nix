f7ecbb592c89a5cabc1ce17cf7dad1a9c15c2d1e
Jinser Kafka <aimer@purejs.icu>
2025-05-11 15:56:50 +0800

    fish(dorothy): function battery highlighted

--- hosts/dorothy/uncat.nix
+++ hosts/dorothy/uncat.nix
@@ -4,7 +4,6 @@
 }:
 
 let
-  inherit (flake.config.symbols.people) myself;
   inherit (flake.config.lib) mkHM;
 in
 mkHM (
@@ -19,7 +18,13 @@ mkHM (
         description = "Show battery info";
         body = # fish
           ''
-            upower -i (upower -e | grep battery)
+            upower -i (upower -e | grep battery) | awk '
+              /state/       {printf "%-15s \033[1;32m%s\033[0m\n",             "State",       $2    };
+              /energy-rate/ {printf "%-15s \033[1;34m%s\033[1;33m%s\033[0m\n", "Energy Rate", $2, $3};
+              /voltage/     {printf "%-15s \033[1;34m%s\033[1;33m%s\033[0m\n", "Voltage",     $2, $3};
+              /percentage/  {printf "%-15s \033[1;34m%s\033[0m\n",             "Percentage",  $2    };
+              /capacity/    {printf "%-15s \033[1;34m%s\033[0m\n",             "Capacity",    $2    };
+            '
           '';
       };
     };
