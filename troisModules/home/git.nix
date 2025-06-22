c7afc6005aceeae91c17ce697b5b90a7efa9d6b7
Jinser Kafka <aimer@purejs.icu>
2025-06-23 01:57:44 +0800

    config(git): configure SSH allowed signers for GPG signing

--- troisModules/home/git.nix
+++ troisModules/home/git.nix
@@ -39,7 +39,10 @@
             };
           };
 
-          gpg.format = "ssh";
+          gpg = {
+            format = "ssh";
+            ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+          };
           user.signingkey = "~/.ssh/id_ed25519.pub";
           commit.gpgsign = true;
 
