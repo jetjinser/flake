39c4ed381285ea38fc1802b1f8418033eec167c2
Jinser Kafka <aimer@purejs.icu>
2025-08-23 17:05:20 +0800

    chabert: native runner

---
+++ hosts/chabert/services/runner.nix
@@ -0,0 +1,40 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+let
+  enable = true;
+  inherit (config.sops) secrets;
+in
+{
+  services.gitea-actions-runner = {
+    package = pkgs.forgejo-runner;
+    instances.local = {
+      inherit enable;
+      url = config.services.forgejo.settings.server.ROOT_URL;
+      tokenFile = secrets.runnerToken.path;
+      name = config.networking.hostName;
+      labels = [ "native:host" ];
+      hostPackages = lib.mkOptionDefault (
+        lib.mkBefore (
+          with pkgs;
+          [
+            # keep-sorted start
+            nushell
+            zuo
+            scsh
+            python3
+            # keep-sorted end
+          ]
+        )
+      );
+    };
+  };
+
+  sops.secrets = {
+    runnerToken = { };
+  };
+}
