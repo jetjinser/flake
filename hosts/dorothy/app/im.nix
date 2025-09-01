d53da856df12bb1bb37e85bf8465b4333628f76d
Jinser Kafka <aimer@purejs.icu>
2025-09-01 14:44:33 +0800

    im(dorothy): QQ IME works

--- hosts/dorothy/app/im.nix
+++ hosts/dorothy/app/im.nix
@@ -6,23 +6,23 @@
 let
   inherit (flake.config.symbols.people) myself;
   inherit (flake.config.lib) mkHM;
-
-  commandLineArgs = [
-    # Force to run on Wayland
-    "--ozone-platform-hint=auto"
-    "--ozone-platform=wayland"
-    "--enable-wayland-ime"
-    # QQ electron does not support v3 at all
-    # "--wayland-text-input-version=3"
-  ];
 in
 mkHM (
   { pkgs, ... }:
   {
     home.packages = with pkgs; [
       telegram-desktop
-      (qq.override { inherit commandLineArgs; })
-      (feishu.override { inherit commandLineArgs; })
+      (qq.override {
+        commandLineArgs = [
+          "--wayland-text-input-version=3"
+        ];
+      })
+      (feishu.override {
+        commandLineArgs = [
+          "--ozone-platform-hint=auto"
+          "--ozone-platform=wayland"
+        ];
+      })
     ];
   }
 )
