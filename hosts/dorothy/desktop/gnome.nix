08930d46bc5c6f7c6e227902eb5b7397d4934265
Jinser Kafka <aimer@purejs.icu>
2025-09-04 22:12:39 +0800

    gnome(dorothy): enable for feishu

--- hosts/dorothy/desktop/gnome.nix
+++ hosts/dorothy/desktop/gnome.nix
@@ -1,8 +1,20 @@
+{
+  pkgs,
+  ...
+}:
+
 let
-  enable = false;
+  enable = true;
 in
 {
   services.desktopManager.gnome = {
     inherit enable;
   };
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+    gnome-user-docs
+  ];
 }
