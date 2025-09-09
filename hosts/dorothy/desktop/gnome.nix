cbec26bfc2a94f2792ca6c4b66c833573e6d4611
Jinser Kafka <aimer@purejs.icu>
2025-09-10 01:00:36 +0800

    gnome(dorothy): exclude whatever random software

--- hosts/dorothy/desktop/gnome.nix
+++ hosts/dorothy/desktop/gnome.nix
@@ -14,7 +14,46 @@ in
   services.gnome.core-developer-tools.enable = false;
   services.gnome.games.enable = false;
   environment.gnome.excludePackages = with pkgs; [
+    # keep-sorted start
+    atomix
+    baobab
+    cheese
+    epiphany
+    evince
+    file-roller
+    geary
+    gnome-backgrounds
+    gnome-calculator
+    gnome-calendar
+    gnome-clocks
+    gnome-connections
+    gnome-contacts
+    gnome-disk-utility
+    gnome-extension-manager
+    gnome-logs
+    gnome-maps
+    gnome-music
+    gnome-photos
+    gnome-software
+    gnome-system-monitor
+    gnome-text-editor
+    gnome-themes-extra
     gnome-tour
     gnome-user-docs
+    gnome-weather
+    hitori
+    iagno
+    localsearch
+    loupe
+    nautilus
+    orca
+    seahorse
+    simple-scan
+    snapshot
+    sushi
+    tali
+    totem
+    yelp
+    # keep-sorted end
   ];
 }
