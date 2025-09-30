0e22d0fdaa0b695d81ddebe93cd5265e888645f8
Jinser Kafka <aimer@purejs.icu>
2025-09-30 21:29:43 +0800

    copilot-api(karenina): enable

---
+++ hosts/karenina/services/llm.nix
@@ -0,0 +1,29 @@
+{
+  flake,
+  config,
+  lib,
+  ...
+}:
+
+let
+  enable = true;
+
+  inherit (config.sops) secrets;
+  cfg = config.services;
+in
+{
+  imports = [ flake.config.modules.nixos.services ];
+
+  sops.secrets = lib.mkIf cfg.copilot-api.enable {
+    gh-token = {
+      owner = config.services.copilot-api.user;
+      inherit (config.services.copilot-api) group;
+      mode = "0400";
+    };
+  };
+  services.copilot-api = {
+    inherit enable;
+    rateLimit = 30;
+    githubTokenFile = secrets.gh-token.path;
+  };
+}
