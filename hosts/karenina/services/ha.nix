f294bc503e0e769eff3a50d64a0645f357e66556
Jinser Kafka <aimer@purejs.icu>
2025-10-19 06:34:28 +0800

    HA(karenina): WIP

--- hosts/karenina/services/ha.nix
+++ hosts/karenina/services/ha.nix
@@ -1,11 +1,12 @@
 {
   config,
+  pkgs,
   lib,
   ...
 }:
 
 let
-  enable = true;
+  enable = false;
   domain = "h.2jk.pw";
 
   inherit (config.sops) secrets;
@@ -13,11 +14,49 @@ let
   cfg = config.services.home-assistant;
 in
 {
+  environment.systemPackages = with pkgs; [ hddtemp ];
+
   services.home-assistant = {
     inherit enable;
+    extraComponents = [
+      # Components required to complete the onboarding
+      "analytics"
+      "google_translate"
+      "met"
+      "radio_browser"
+      "shopping_list"
+      # Recommended for fast zlib compression
+      # https://www.home-assistant.io/integrations/isal
+      "isal"
+
+      # keep-sorted start
+      "ai_task"
+      "date"
+      "device_tracker"
+      "media_player"
+      "mobile_app"
+      "moon"
+      "open_router"
+      "plex"
+      "raspberry_pi"
+      "remote_calendar"
+      "spotify"
+      "sun"
+      "tailscale"
+      "telegram"
+      "telegram_bot"
+      "xiaomi"
+      # keep-sorted end
+    ];
+    customComponents = builtins.attrValues (
+      lib.packagesFromDirectoryRecursive {
+        inherit (pkgs) callPackage;
+        directory = ../ha-components;
+      }
+    );
     config = {
+      # lovelace.mode = "yaml";
       http = {
-        base_url = "http://${domain}";
         server_host = [ "127.0.0.1" ];
         use_x_forwarded_for = true;
         trusted_proxies = [ "127.0.0.1" ];
@@ -26,6 +65,106 @@ in
         name = hostName;
         unit_system = "metric";
       };
+      device_tracker = [
+        {
+          platform = "xiaomi";
+          host = throw "TODO";
+          password = throw "TODO";
+        }
+      ];
+    };
+    customLovelaceModules = with pkgs.home-assistant-custom-lovelace-modules; [
+      auto-entities
+      bubble-card
+    ];
+    lovelaceConfig = {
+      title = "JK@Home";
+      views = [
+        {
+          type = "sections";
+          title = "宿舍";
+          path = "dormitory";
+          icon = "mdi:home-assistant";
+          badges = [
+            {
+              type = "entity";
+              entity = "sensor.backup_backup_manager_state";
+              color = "red";
+            }
+            {
+              type = "entity";
+              entity = "sensor.backup_backup_manager_state";
+              color = "indigo";
+            }
+            {
+              type = "entity";
+              entity = "device_tracker.nixos";
+            }
+          ];
+          sections = [
+            {
+              cards = [
+                {
+                  type = "heading";
+                  heading = "屋内";
+                  icon = "mdi:ceiling-light";
+                  badges = [
+                    {
+                      type = "entity";
+                      entity = "sensor.backup_backup_manager_state";
+                      show_state = false;
+                      color = "red";
+                    }
+                  ];
+                }
+                {
+                  type = "weather-forecast";
+                  entity = "weather.forecast_wo_de_jia";
+                }
+                {
+                  type = "custom:auto-entities";
+                  card = {
+                    type = "entities";
+                    title = "路由器连接设备";
+                  };
+                  filter = {
+                    include = [
+                      {
+                        domain = "device_tracker";
+                        attributes = {
+                          scanner = "XiaomiDeviceScanner";
+                        };
+                      }
+                    ];
+                  };
+                }
+              ];
+            }
+            {
+              cards = [
+                {
+                  type = "heading";
+                  heading = "桌前";
+                  icon = "mdi:desk-lamp";
+                  badges = [
+                    {
+                      type = "entity";
+                      entity = "sensor.backup_backup_manager_state";
+                      show_state = false;
+                      color = "blue";
+                    }
+                  ];
+                }
+                {
+                  type = "markdown";
+                  title = "TODO";
+                  content = "Placeholder";
+                }
+              ];
+            }
+          ];
+        }
+      ];
     };
   };
 
