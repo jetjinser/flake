b5207f6cd912088d8b77e6cbcccb7391a51ff77c
Jinser Kafka <aimer@purejs.icu>
2025-10-27 01:45:12 +0800

    seaweedfs.volume(sheep): init

---
+++ hosts/sheep/services/dfs.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  ...
+}:
+
+let
+  cfg = config.services.seaweedfs;
+
+  enable = true;
+  domain = "mie.2jk.pw";
+  ipBind = "0.0.0.0";
+  master = [
+    "anna.2jk.pw:${toString cfg.master.optionsCLI.port}"
+  ];
+in
+{
+  services.seaweedfs = {
+    inherit enable;
+    openFirewall = true;
+    volume = {
+      enable = true;
+      optionsCLI = {
+        ip = domain;
+        mserver = master;
+        "ip.bind" = ipBind;
+        dataCenter = "MIE";
+        rack = config.networking.hostName;
+        dir = "/vol.data";
+        "dir.idx" = "/vol.meta";
+        disk = "ssd";
+        max = 128;
+      };
+    };
+  };
+}
