14f14ff5dda969a14d1925af1abcec84d5e182ce
Jinser Kafka <aimer@purejs.icu>
2025-11-01 02:10:40 +0800

    nh(bendemann): enable

--- hosts/bendemann/default.nix
+++ hosts/bendemann/default.nix
@@ -3,6 +3,9 @@
   ...
 }:
 
+let
+  inherit (flake.config.symbols.people) myself;
+in
 {
   imports = [
     # ../../troisModules/nixos/default.nix
@@ -24,4 +27,12 @@
   ];
 
   nix.channel.enable = false;
+
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/${myself}/vie/projet/flake";
+  };
+  nix.gc.automatic = false;
 }
