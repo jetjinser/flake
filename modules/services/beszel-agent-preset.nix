014e8ff978a633139043d73cfe3afc8f888b5a35
Jinser Kafka <aimer@purejs.icu>
2025-11-16 18:24:54 +0800

    module(beszel-hub-preset): init

---
+++ modules/services/beszel-agent-preset.nix
@@ -0,0 +1,50 @@
+{
+  flake,
+  config,
+  lib,
+  ...
+}:
+
+let
+  hub-cfg = flake.self.nixosConfigurations.cosimo.config.services.beszel.hub;
+  cfg = config.services.beszel-agent-preset;
+in
+{
+  options.services.beszel-agent-preset = {
+    enable = lib.mkEnableOption "Enable the beszel agent (preset) service.";
+
+    tokenPath = lib.mkOption {
+      type = lib.types.path;
+      description = "Path to token.";
+    };
+    hubUrl = lib.mkOption {
+      type = lib.types.str;
+      default = "http://${hub-cfg.host}:${toString hub-cfg.port}";
+    };
+    key = lib.mkOption {
+      type = lib.types.str;
+    };
+
+    agentEnv = lib.mkOption {
+      type = lib.types.attrs;
+      default = {
+        LOG_LEVEL = "warn";
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.beszel.agent = {
+      enable = true;
+      environment = {
+        HUB_URL = cfg.hubUrl;
+        KEY = cfg.key;
+        # TOKEN_FILE = "$CREDENTIALS_DIRECTORY/hubToken";
+        TOKEN_FILE = "/run/credentials/beszel-agent.service/hubToken";
+      }
+      // cfg.agentEnv;
+    };
+
+    systemd.services.beszel-agent.serviceConfig.LoadCredential = "hubToken:${cfg.tokenPath}";
+  };
+}
