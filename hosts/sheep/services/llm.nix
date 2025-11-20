371b02639e84933a66ec1f5ff1ab2fe9ac43f839
Jinser Kafka <aimer@purejs.icu>
2025-11-20 15:51:32 +0800

    karakeep(chabert): migrated

---
+++ hosts/sheep/services/llm.nix
@@ -0,0 +1,21 @@
+let
+  enable = true;
+in
+{
+  services = {
+    ollama = {
+      inherit enable;
+      loadModels = [
+        "qwen3:4b"
+        # embedding models
+        "embeddinggemma:300m"
+      ];
+    };
+  };
+
+  # broken: https://github.com/NixOS/nixpkgs/pull/367695
+  # nixpkgs.config = {
+  #   cudaSupport = false;
+  #   rocmSupport = true;
+  # };
+}
