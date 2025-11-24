4f6e54d8a7e1bff04c33f4f299651d5e12daaa15
Jinser Kafka <aimer@purejs.icu>
2025-11-24 10:10:57 +0800

    beszel: migrate & wip

--- hosts/karenina/services/beszel-agent.nix
+++ hosts/karenina/services/beszel-agent.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  enable = true;
+  enable = false;
 
   inherit (config.sops) secrets;
   cfg = config.services.beszel-agent-preset;
@@ -14,7 +14,7 @@ in
   services.beszel-agent-preset = {
     inherit enable;
     tokenPath = secrets.hubToken.path;
-    key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILAto51XPlgJ9xlsNRxHiRQEjyblK1JAO+54nGjLj/uk";
+    key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAl+ILrZZwMAYH/x628CxX76/MeO1xMUPvbFQmy4LK0c";
   };
 
   sops.secrets = lib.mkIf cfg.enable {
--- hosts/sheep/services/beszel-agent.nix
+++ hosts/sheep/services/beszel-agent.nix
@@ -14,7 +14,7 @@ in
   services.beszel-agent-preset = {
     inherit enable;
     tokenPath = secrets.hubToken.path;
-    key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILAto51XPlgJ9xlsNRxHiRQEjyblK1JAO+54nGjLj/uk";
+    key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAl+ILrZZwMAYH/x628CxX76/MeO1xMUPvbFQmy4LK0c";
     agentEnv = {
       EXTRA_FILESYSTEMS = "/persist";
     };
