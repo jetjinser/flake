c4249e0a2e6c1d50b1df9998e03c62dea2c36596
Jinser Kafka <aimer@purejs.icu>
2025-12-21 00:01:59 +0800

    transmission(karenina): disable

--- hosts/karenina/services/dfs.nix
+++ hosts/karenina/services/dfs.nix
@@ -16,85 +16,88 @@ let
   ];
 in
 {
-  services.seaweedfs = {
-    inherit enable;
-    openFirewall = true;
-    settings = {
+  config = lib.mkIf enable {
+    services.seaweedfs = {
+      inherit enable;
+      openFirewall = true;
+      settings = {
+        master = {
+          master.maintenance = {
+            scripts = ''
+              lock
+              ec.encode -fullPercent=95 -quietFor=1h
+              ec.rebuild -force
+              ec.balance -force
+              volume.deleteEmpty -quietFor=24h -force
+              volume.balance -force
+              volume.fix.replication -force
+              s3.clean.uploads -timeAgo=24h
+              unlock
+            '';
+            sleep_minutes = 17;
+          };
+        };
+      };
       master = {
-        master.maintenance = {
-          scripts = ''
-            lock
-            ec.encode -fullPercent=95 -quietFor=1h
-            ec.rebuild -force
-            ec.balance -force
-            volume.deleteEmpty -quietFor=24h -force
-            volume.balance -force
-            volume.fix.replication -force
-            s3.clean.uploads -timeAgo=24h
-            unlock
-          '';
-          sleep_minutes = 17;
+        enable = true;
+        optionsCLI = {
+          ip = domain;
+          "ip.bind" = ipBind;
+          # 1GB
+          volumeSizeLimitMB = 1024;
+          volumePreallocate = true;
+        };
+      };
+      volume = {
+        enable = true;
+        optionsCLI = {
+          ip = domain;
+          inherit master;
+          "ip.bind" = ipBind;
+          dataCenter = "UNI";
+          rack = config.networking.hostName;
+          dir = "/srv/data/seaweedfs/volume";
+          max = 30;
+        };
+      };
+      filer = {
+        enable = true;
+        optionsCLI = {
+          ip = domain;
+          inherit master;
+          "ip.bind" = ipBind;
+          # s3 = true;
+          webdav = true;
+          "webdav.collection" = "h";
+          "webdav.filer.path" = "/staging";
+          dataCenter = "UNI";
+          rack = config.networking.hostName;
+          encryptVolumeData = true;
         };
       };
     };
-    master = {
-      enable = true;
-      optionsCLI = {
-        ip = domain;
-        "ip.bind" = ipBind;
-        # 1GB
-        volumeSizeLimitMB = 1024;
-        volumePreallocate = true;
-      };
-    };
-    volume = {
-      enable = true;
-      optionsCLI = {
-        ip = domain;
-        inherit master;
-        "ip.bind" = ipBind;
-        dataCenter = "UNI";
-        rack = config.networking.hostName;
-        dir = "/srv/data/seaweedfs/volume";
-        max = 30;
-      };
-    };
-    filer = {
-      enable = true;
-      optionsCLI = {
-        ip = domain;
-        inherit master;
-        "ip.bind" = ipBind;
-        # s3 = true;
-        webdav = true;
-        "webdav.collection" = "h";
-        "webdav.filer.path" = "/staging";
-        dataCenter = "UNI";
-        rack = config.networking.hostName;
-        encryptVolumeData = true;
+
+    services.caddy = {
+      virtualHosts = lib.mkIf cfg.enable {
+        "fs.2jk.pw" = {
+          extraConfig = ''
+            tls ${../../../assets/karenina.crt} ${secrets.karenina-key.path}
+            reverse_proxy http://${ipBind}:${toString cfg.filer.optionsCLI.port} {
+              header_down X-Real-IP {http.request.remote}
+              header_down X-Forwarded-For {http.request.remote}
+            }
+          '';
+        };
+        "http://dav.2jk.pw" = {
+          extraConfig = ''
+            reverse_proxy http://${ipBind}:${toString cfg.filer.optionsCLI."webdav.port"} {
+              header_down X-Real-IP {http.request.remote}
+              header_down X-Forwarded-For {http.request.remote}
+            }
+          '';
+        };
       };
     };
   };
 
-  services.caddy = {
-    virtualHosts = lib.mkIf cfg.enable {
-      "fs.2jk.pw" = {
-        extraConfig = ''
-          tls ${../../../assets/karenina.crt} ${secrets.karenina-key.path}
-          reverse_proxy http://${ipBind}:${toString cfg.filer.optionsCLI.port} {
-            header_down X-Real-IP {http.request.remote}
-            header_down X-Forwarded-For {http.request.remote}
-          }
-        '';
-      };
-      "http://dav.2jk.pw" = {
-        extraConfig = ''
-          reverse_proxy http://${ipBind}:${toString cfg.filer.optionsCLI."webdav.port"} {
-            header_down X-Real-IP {http.request.remote}
-            header_down X-Forwarded-For {http.request.remote}
-          }
-        '';
-      };
-    };
-  };
 }
--- hosts/karenina/services/downloader.nix
+++ hosts/karenina/services/downloader.nix
@@ -11,130 +11,133 @@ let
   inherit (config.sops) secrets;
   inherit (flake.config.symbols.people) myself;
 
-  enable = true;
+  enable = false;
   download-dir = "/srv/staging";
 in
 {
-  services.transmission =
-    let
-      autoUnar = pkgs.writeShellApplication {
-        name = "auto-unar";
-        runtimeInputs = with pkgs; [
-          bash
-          findutils
-          unar
-        ];
-        text = ''
-          find /"$TR_TORRENT_DIR"/"$TR_TORRENT_NAME" \
-            -type f -iregex '.*\.\(zip\|tar.bz\|rar\|tar.gz\|7z\)$' \
-            -execdir bash -c 'unar "$0" && echo "would rm $0 when seeding done" || echo "failed: $0"' {} \;
+  config = lib.mkIf enable {
+    services.transmission =
+      let
+        autoUnar = pkgs.writeShellApplication {
+          name = "auto-unar";
+          runtimeInputs = with pkgs; [
+            bash
+            findutils
+            unar
+          ];
+          text = ''
+            find /"$TR_TORRENT_DIR"/"$TR_TORRENT_NAME" \
+              -type f -iregex '.*\.\(zip\|tar.bz\|rar\|tar.gz\|7z\)$' \
+              -execdir bash -c 'unar "$0" && echo "would rm $0 when seeding done" || echo "failed: $0"' {} \;
+          '';
+        };
+      in
+      {
+        # TODO: use qbittorent?
+        inherit enable;
+        package = pkgs.transmission_4;
+        openRPCPort = true;
+        # on my local machine without public IPv4 IP
+        openPeerPorts = true;
+        # https://github.com/NixOS/nixpkgs/pull/371241
+        webHome = pkgs.flood-for-transmission;
+        settings = {
+          rpc-port = 9001;
+          rpc-bind-address = "127.0.0.1";
+          rpc-whitelist-enabled = false;
+          rpc-username = myself;
+          rpc-password = "{2b79a09b99bc2b99da06665666853bd337052a05ypW43WFG";
+          rpc-authentication-required = true;
+
+          # default
+          peer-port = 51413;
+
+          inherit download-dir;
+          download-queue-size = 10; # default to 5
+          incomplete-dir-enabled = true;
+          ldp-enabled = true;
+          start-added-torrents = false;
+
+          speed-limit-up-enabled = true;
+          speed-limit-up = 800; # 800 KB/s
+          alt-speed-enabled = true;
+          alt-speed-up = 450; # 450 KB/s
+          alt-speed-down = 10000000; # 10 GB/s
+          alt-speed-time-enabled = true;
+          alt-speed-time-begin = 480; # 8 AM
+          lat-speed-time-end = 60; # 1 AM
+
+          script-torrent-done-enabled = false;
+          script-torrent-done-filename = lib.getExe autoUnar;
+
+          # 0 = Prefer unencrypted connections
+          # 1 = Prefer encrypted connections
+          # 2 = Require encrypted connections;
+          # default = 1
+          # Encryption may help get around some ISP filtering,
+          # but at the cost of slightly higher CPU use
+          # > Xunlei does not have enc
+          encryption = 2;
+        };
+      };
+    networking.firewall = {
+      allowedTCPPorts = [
+        cfg.settings.peer-port
+        # UPnP
+        5000
+      ];
+      allowedUDPPorts = [
+        cfg.settings.peer-port
+        # NAT-PMP/PCP
+        5351
+      ];
+    };
+
+    # services.peer-ban-helper = {
+    #   inherit (cfg) enable;
+    #   address = "0.0.0.0";
+    #   port = 9898;
+    # };
+
+    fileSystems."/srv/staging" = lib.mkIf cfg.enable {
+      device = "fuse";
+      fsType = "fuse./run/current-system/sw/bin/weed";
+      options = [
+        "_netdev"
+        "filer=fs.2jk.pw:8888"
+        "filer.path=/staging"
+        "collection=h"
+        "X-mount.owner=transmission"
+        "X-mount.group=users"
+      ];
+    };
+
+    systemd.tmpfiles.settings.downloaded = lib.mkIf cfg.enable {
+      "${cfg.settings.download-dir}".d = {
+        inherit (cfg) user;
+        group = "users";
+        mode = "0775";
+      };
+    };
+
+    services.caddy = {
+      virtualHosts = lib.mkIf cfg.enable {
+        "tm.2jk.pw".extraConfig = ''
+          tls ${../../../assets/karenina.crt} ${secrets.karenina-key.path}
+          reverse_proxy http://${cfg.settings.rpc-bind-address}:${toString cfg.settings.rpc-port} {
+            header_down X-Real-IP {http.request.remote}
+            header_down X-Forwarded-For {http.request.remote}
+          }
+        '';
+        "ltm.2jk.pw".extraConfig = ''
+          tls ${../../../assets/karenina.crt} ${secrets.karenina-key.path}
+          reverse_proxy http://${cfg.settings.rpc-bind-address}:${toString cfg.settings.rpc-port} {
+            header_down X-Real-IP {http.request.remote}
+            header_down X-Forwarded-For {http.request.remote}
+          }
         '';
       };
-    in
-    {
-      # TODO: use qbittorent?
-      inherit enable;
-      package = pkgs.transmission_4;
-      openRPCPort = true;
-      # on my local machine without public IPv4 IP
-      openPeerPorts = true;
-      # https://github.com/NixOS/nixpkgs/pull/371241
-      webHome = pkgs.flood-for-transmission;
-      settings = {
-        rpc-port = 9001;
-        rpc-bind-address = "127.0.0.1";
-        rpc-whitelist-enabled = false;
-        rpc-username = myself;
-        rpc-password = "{2b79a09b99bc2b99da06665666853bd337052a05ypW43WFG";
-        rpc-authentication-required = true;
-
-        # default
-        peer-port = 51413;
-
-        inherit download-dir;
-        download-queue-size = 10; # default to 5
-        incomplete-dir-enabled = true;
-        ldp-enabled = true;
-        start-added-torrents = false;
-
-        speed-limit-up-enabled = true;
-        speed-limit-up = 800; # 800 KB/s
-        alt-speed-enabled = true;
-        alt-speed-up = 450; # 450 KB/s
-        alt-speed-down = 10000000; # 10 GB/s
-        alt-speed-time-enabled = true;
-        alt-speed-time-begin = 480; # 8 AM
-        lat-speed-time-end = 60; # 1 AM
-
-        script-torrent-done-enabled = false;
-        script-torrent-done-filename = lib.getExe autoUnar;
-
-        # 0 = Prefer unencrypted connections
-        # 1 = Prefer encrypted connections
-        # 2 = Require encrypted connections;
-        # default = 1
-        # Encryption may help get around some ISP filtering,
-        # but at the cost of slightly higher CPU use
-        # > Xunlei does not have enc
-        encryption = 2;
-      };
     };
-  networking.firewall = {
-    allowedTCPPorts = [
-      cfg.settings.peer-port
-      # UPnP
-      5000
-    ];
-    allowedUDPPorts = [
-      cfg.settings.peer-port
-      # NAT-PMP/PCP
-      5351
-    ];
-  };
 
-  # services.peer-ban-helper = {
-  #   inherit (cfg) enable;
-  #   address = "0.0.0.0";
-  #   port = 9898;
-  # };
-
-  fileSystems."/srv/staging" = lib.mkIf cfg.enable {
-    device = "fuse";
-    fsType = "fuse./run/current-system/sw/bin/weed";
-    options = [
-      "_netdev"
-      "filer=fs.2jk.pw:8888"
-      "filer.path=/staging"
-      "collection=h"
-      "X-mount.owner=transmission"
-      "X-mount.group=users"
-    ];
-  };
-
-  systemd.tmpfiles.settings.downloaded = lib.mkIf cfg.enable {
-    "${cfg.settings.download-dir}".d = {
-      inherit (cfg) user;
-      group = "users";
-      mode = "0775";
-    };
-  };
-
-  services.caddy = {
-    virtualHosts = lib.mkIf cfg.enable {
-      "tm.2jk.pw".extraConfig = ''
-        tls ${../../../assets/karenina.crt} ${secrets.karenina-key.path}
-        reverse_proxy http://${cfg.settings.rpc-bind-address}:${toString cfg.settings.rpc-port} {
-          header_down X-Real-IP {http.request.remote}
-          header_down X-Forwarded-For {http.request.remote}
-        }
-      '';
-      "ltm.2jk.pw".extraConfig = ''
-        tls ${../../../assets/karenina.crt} ${secrets.karenina-key.path}
-        reverse_proxy http://${cfg.settings.rpc-bind-address}:${toString cfg.settings.rpc-port} {
-          header_down X-Real-IP {http.request.remote}
-          header_down X-Forwarded-For {http.request.remote}
-        }
-      '';
-    };
   };
 }
