e531b91389240922d6dc629d046eda75426c4d87
Jinser Kafka <aimer@purejs.icu>
2026-02-04 12:16:09 +0800

    template: chicken

---
+++ templates/chicken/nix/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A startup Chicken project with devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.chicken
+        ]
+        ++ (with pkgs.chickenPackages.chickenEggs; [
+          srfi-13
+        ]);
+      };
+    };
+}
