e36a5b3bebe6cc8e3461207779c9ec88bf4ba96d
Jinser Kafka <aimer@purejs.icu>
2026-05-09 16:55:16 +0800

    battery(dorothy): update awk

--- hosts/dorothy/uncat.nix
+++ hosts/dorothy/uncat.nix
@@ -15,11 +15,11 @@ mkHM (_: {
       body = # fish
         ''
           upower -i (upower -e | grep battery) | awk '
-            /state/       {printf "%-15s \033[1;32m%s\033[0m\n",             "State",       $2    };
-            /energy-rate/ {printf "%-15s \033[1;34m%s\033[1;33m%s\033[0m\n", "Energy Rate", $2, $3};
-            /voltage/     {printf "%-15s \033[1;34m%s\033[1;33m%s\033[0m\n", "Voltage",     $2, $3};
-            /percentage/  {printf "%-15s \033[1;34m%s\033[0m\n",             "Percentage",  $2    };
-            /capacity/    {printf "%-15s \033[1;34m%s\033[0m\n",             "Capacity",    $2    };
+            /state:/       {printf "%-15s \033[1;32m%s\033[0m\n",             "State",       $2    };
+            /energy-rate:/ {printf "%-15s \033[1;34m%s\033[1;33m%s\033[0m\n", "Energy Rate", $2, $3};
+            /voltage:/     {printf "%-15s \033[1;34m%s\033[1;33m%s\033[0m\n", "Voltage",     $2, $3};
+            /percentage:/  {printf "%-15s \033[1;34m%s\033[0m\n",             "Percentage",  $2    };
+            /capacity:/    {printf "%-15s \033[1;34m%s\033[0m\n",             "Capacity",    $2    };
           '
         '';
     };
