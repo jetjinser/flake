b7487a36fa406fe91c8795f11a7b51a805ec054b
Jinser Kafka <aimer@purejs.icu>
2026-05-09 16:54:51 +0800

    chore(sheep): mc

--- hosts/sheep/services/mc.nix
+++ hosts/sheep/services/mc.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  enable = false;
+  enable = true;
 
   inherit (flake.inputs) nix-minecraft;
 in
@@ -237,48 +237,48 @@ in
         };
       };
     };
-    networking.firewall.allowedUDPPorts = [ 38814 ];
-
-    services.cloudflared' = {
-      # Minecraft BlueMap plugin listen port
-      ingress.map = 8101;
-    };
-
-    systemd.services.caddy.serviceConfig.SupplementaryGroups = [ "minecraft" ];
-    services.caddy.virtualHosts = {
-      ":8101" = {
-        extraConfig =
-          # caddy
-          ''
-            basicauth {
-              ghost $2a$14$X0OKNB6d0KeXLQyMW2qNwuiIzs6vEfAoptHTTef8Ki.uY6D2Im1Xy
-            }
-
-            root * /var/lib/minecraft/p1/bluemap/web
-            file_server
-
-            header Cache-Control "public, max-age=31536000, immutable"
-
-            # Match the textures.json file & .prbm files
-            @gz path /maps/*/textures.json *.prbm
-            # Find .gz files (if not found respond with 204) for the above matcher, and set the "Content-Encoding gzip" header
-            handle @gz {
-              try_files {path}.gz =204
-              header Content-Encoding gzip
-            }
-
-            # Respond with 204 for non-existant map-tiles
-            @204 path */tiles/*
-            handle @204 {
-              try_files {path} =204
-            }
-
-            # Proxy requests for live data to the bluemaps integrated webserver.
-            handle /maps/*/live/* {
-              reverse_proxy 127.0.0.1:8100
-            }
-          '';
-      };
-    };
+    # networking.firewall.allowedUDPPorts = [ 38814 ];
+    #
+    # services.cloudflared' = {
+    #   # Minecraft BlueMap plugin listen port
+    #   ingress.map = 8101;
+    # };
+    #
+    # systemd.services.caddy.serviceConfig.SupplementaryGroups = [ "minecraft" ];
+    # services.caddy.virtualHosts = {
+    #   ":8101" = {
+    #     extraConfig =
+    #       # caddy
+    #       ''
+    #         basicauth {
+    #           ghost $2a$14$X0OKNB6d0KeXLQyMW2qNwuiIzs6vEfAoptHTTef8Ki.uY6D2Im1Xy
+    #         }
+    #
+    #         root * /var/lib/minecraft/p1/bluemap/web
+    #         file_server
+    #
+    #         header Cache-Control "public, max-age=31536000, immutable"
+    #
+    #         # Match the textures.json file & .prbm files
+    #         @gz path /maps/*/textures.json *.prbm
+    #         # Find .gz files (if not found respond with 204) for the above matcher, and set the "Content-Encoding gzip" header
+    #         handle @gz {
+    #           try_files {path}.gz =204
+    #           header Content-Encoding gzip
+    #         }
+    #
+    #         # Respond with 204 for non-existant map-tiles
+    #         @204 path */tiles/*
+    #         handle @204 {
+    #           try_files {path} =204
+    #         }
+    #
+    #         # Proxy requests for live data to the bluemaps integrated webserver.
+    #         handle /maps/*/live/* {
+    #           reverse_proxy 127.0.0.1:8100
+    #         }
+    #       '';
+    #   };
+    # };
   };
 }
