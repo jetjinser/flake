7126cf3a89ae874bfbfd30bb9332d36e558504ef
Jinser Kafka <aimer@purejs.icu>
2026-05-29 19:15:05 +0800

    caddy(sheep): update

--- hosts/sheep/services/default.nix
+++ hosts/sheep/services/default.nix
@@ -49,7 +49,7 @@ in
     enable = cfg.caddy.virtualHosts != { };
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-      hash = "sha256-dNmccsad8nFm2X9w/6mdgAdhPSquNm6oVhf3sNc46SQ=";
+      hash = "sha256-qEA6058svI8Q6yE97OkfnGWC8ayI3x8y2iU7PGkJ3Do=";
     };
     environmentFile = secrets.caddy.path;
     virtualHosts."(tsnet)".extraConfig = ''
