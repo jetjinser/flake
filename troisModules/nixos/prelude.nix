aaad77eb6e39e242a57fd9315ab36cad2d49dc27
Jinser Kafka <aimer@purejs.icu>
2026-06-16 11:15:48 +0800

    no default git

--- troisModules/nixos/prelude.nix
+++ troisModules/nixos/prelude.nix
@@ -2,35 +2,62 @@
   pkgs,
   lib,
   flake,
+  config,
   ...
 }:
 
+let
+  cfg = config.customize.prelude;
+in
 {
   imports = [
     flake.inputs.nix-topology.nixosModules.default
   ];
 
-  environment.systemPackages = map lib.lowPrio (
-    with pkgs;
-    [
-      # keep-sorted start
-      curl
-      file
-      git
-      jq
-      screen
-      zuo # maybe Guile?
-      # keep-sorted end
-    ]
-  );
+  options.customize.prelude = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      example = false;
+      description = "Whether to enable prelude.";
+    };
+    withGit = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      example = false;
+      description = "Whether to include gitMinimal.";
+    };
 
-  nixpkgs.overlays = [
-    (
-      _final: prev:
-      prev.lib.packagesFromDirectoryRecursive {
-        inherit (prev) callPackage;
-        directory = ../../modules/pkgs;
-      }
-    )
-  ];
+    pkgs = lib.mkOption {
+      type = with lib.types; listOf package;
+      default =
+        (with pkgs; [
+          # keep-sorted start
+          curl
+          file
+          jq
+          screen
+          zuo # maybe Guile?
+          # keep-sorted end
+        ])
+        ++ lib.optional cfg.withGit [
+          pkgs.gitMinimal
+        ];
+      description = "Packages included.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = map lib.lowPrio (cfg.pkgs);
+
+    nixpkgs.overlays = [
+      (
+        _final: prev:
+        prev.lib.packagesFromDirectoryRecursive {
+          inherit (prev) callPackage;
+          directory = ../../modules/pkgs;
+        }
+      )
+    ];
+  };
 }
