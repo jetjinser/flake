238fe8e2657321213ae49846ed350736f58f02b3
Jinser Kafka <aimer@purejs.icu>
2026-06-16 12:02:11 +0800

    enable ipv6 discovery on sheep

--- hosts/sheep/network.nix
+++ hosts/sheep/network.nix
@@ -12,23 +12,32 @@ in
 {
   services.openssh.ports = lib.mkForce [ 38814 ];
 
+  boot.kernel.sysctl = {
+    "net.ipv6.conf.ens18.forwarding" = "0";
+  };
   networking = {
     hostName = "miecloud";
 
     useDHCP = false;
+    enableIPv6 = true;
 
     interfaces = {
-      ens18.ipv4.addresses = [
-        {
-          address = "192.168.114.72";
-          prefixLength = 24;
-        }
-      ];
+      ens18 = {
+        ipv4.addresses = [
+          {
+            address = "192.168.114.72";
+            prefixLength = 24;
+          }
+        ];
+        ipv6.addresses = [ ];
+      };
     };
     defaultGateway = {
       address = "192.168.114.254";
       interface = "ens18";
     };
+
+    tempAddresses = "enabled";
 
     nameservers = [
       "119.29.29.29"
