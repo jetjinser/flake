df7ed8786812e7a26bce4a959656bb557fcfb809
Jinser Kafka <aimer@purejs.icu>
2026-08-04 02:37:15 +0800

    update sing-box config

--- hosts/dorothy/services/proxy.nix
+++ hosts/dorothy/services/proxy.nix
@@ -13,18 +13,18 @@ let
   };
   secretGenerator = topic: ss: (lib.genAttrs ss (mkSecret topic));
 
-  proxy-final = "proxy.dc99";
+  proxy-final = "proxy.g12-6";
 
   sing-box = pkgs.sing-box.overrideAttrs (
     finalAttrs: _prevAttrs: {
-      version = "1.14.0-alpha.25";
+      version = "1.14.0-alpha.30";
       src = pkgs.fetchFromGitHub {
         owner = "SagerNet";
         repo = "sing-box";
         tag = "v${finalAttrs.version}";
-        sha256 = "sha256-dAfYgKYx3nn1bNI5MnnA9VjsOg/XSJtfJwB3YIfDxN0=";
+        sha256 = "sha256-r/NRt2ndi4k51VLDTPEyALe45GBOiMSbDuPBLkDbbn4=";
       };
-      vendorHash = "sha256-W3xMbClnDrpTcxM8Lkc2lud4xX5MmHcT10/7WBNPXlc=";
+      vendorHash = "sha256-CuZS+9dwGTkoE9aL1Ua9IGm0wQFfA/5U5nms4TchVvI=";
     }
   );
 in
@@ -33,15 +33,15 @@ in
     server-g12-6 = { };
     password-g12-6 = { };
     method-g12-6 = { };
-    server-dc99 = { };
-    password-dc99 = { };
-    method-dc99 = { };
+    server-colo = { };
+    password-colo = { };
+    method-colo = { };
     server-mj = { };
     uuid-mj = { };
     Host-mj = { };
-    server-bwh99 = { };
-    username-bwh99 = { };
-    password-bwh99 = { };
+    server-green = { };
+    password-green = { };
+    method-green = { };
   };
   sops.secrets.tailscaleAuthKey = { };
 
@@ -75,13 +75,25 @@ in
           "uuid"
         ])
       ];
-      proxy-dc99 = lib.mergeAttrsList [
+      proxy-colo = lib.mergeAttrsList [
         {
           type = "shadowsocks";
-          tag = "proxy.dc99";
-          server_port = 29137;
+          tag = "proxy.colo";
+          server_port = 30880;
         }
-        (secretGenerator "dc99" [
+        (secretGenerator "colo" [
+          "server"
+          "password"
+          "method"
+        ])
+      ];
+      proxy-green = lib.mergeAttrsList [
+        {
+          type = "shadowsocks";
+          tag = "proxy.green";
+          server_port = 2544;
+        }
+        (secretGenerator "green" [
           "server"
           "password"
           "method"
@@ -92,29 +104,20 @@ in
       enable = true;
       package = sing-box;
       settings = {
-        log.level = "info";
+        log.level = "warn";
         dns = {
           servers = [
-            # {
-            #   type = "udp";
-            #   tag = "cf";
-            #   server = "1.1.1.1";
-            #   detour = proxy-final;
-            # }
-            # {
-            #   type = "dhcp";
-            #   tag = "cf";
-            # }
-            # {
-            #   type = "h3";
-            #   tag = "cf";
-            #   server = "cloudflare-dns.com";
-            #   domain_resolver = "ali";
-            #   detour = proxy-final;
-            # }
+            {
+              type = "https";
+              tag = "dns-remote";
+              server = "1.1.1.1";
+              path = "/dns-query";
+              detour = proxy-final;
+            }
             {
               type = "local";
-              tag = "cf";
+              tag = "dns-local";
+              detour = "direct-out";
             }
             {
               type = "tailscale";
@@ -123,19 +126,30 @@ in
             }
           ];
           rules = [
-            # {
-            #   preferred_by = "ts";
-            #   action = "route";
-            #   server = "ts";
-            # }
             {
               action = "route";
               domain_suffix = [ ".ts.net" ];
               preferred_by = "ts";
               server = "ts";
             }
+            {
+              action = "route";
+              rule_set = [ "geosite-cn" ];
+              server = "dns-local";
+            }
+            {
+              action = "route";
+              domain_suffix = [
+                ".2jk.pw"
+                ".bhu.social"
+                ".zoom.us"
+                "spritely.institute"
+              ];
+              server = "dns-local";
+            }
           ];
-          final = "cf";
+          strategy = "ipv4_only";
+          final = "dns-remote";
         };
         endpoints = [
           {
@@ -147,8 +161,8 @@ in
         ];
         inbounds = [
           {
-            type = "http";
-            tag = "http-in";
+            type = "mixed";
+            tag = "mixed-in";
             listen = "::";
             listen_port = 7890;
           }
@@ -158,24 +172,24 @@ in
             interface_name = "singtun0";
             address = [
               "172.18.0.1/30"
-              # "fdfe:dcba:9876::1/126"
+              "fdfe:dcba:9876::1/126"
             ];
             auto_route = true;
             auto_redirect = true;
             strict_route = true;
+            stack = "mixed";
             dns_mode = "hijack";
-            # dns_address = [ "100.100.100.100" ];
           }
         ];
         outbounds = [
           proxy-g12-6
           proxy-mj
-          proxy-dc99
-          # proxy-bwh99
+          proxy-colo
+          proxy-green
           {
             type = "direct";
             tag = "direct-out";
-            domain_resolver = "cf";
+            domain_resolver = "dns-local";
           }
         ];
         route = {
@@ -187,6 +201,7 @@ in
               protocol = "dns";
             }
             {
+              action = "route";
               outbound = "ts-ep";
               ip_cidr = "100.64.0.0/10";
             }
@@ -200,6 +215,7 @@ in
                 "gstatic.com"
                 "googletagmanager.com"
                 "kimi.com"
+                "livehouse.2jk.pw"
               ];
             }
             {
@@ -212,7 +228,7 @@ in
                   domain_suffix = [
                     ".2jk.pw"
                     ".bhu.social"
-                    ".purejs.icu"
+                    # ".purejs.icu"
                     ".zoom.us"
                     "spritely.institute"
                   ];
@@ -264,7 +280,7 @@ in
             });
           final = proxy-final;
           auto_detect_interface = true;
-          default_domain_resolver = "cf";
+          default_domain_resolver = "dns-remote";
         };
       };
     };
