263b79683806d5ee423fc73d9d722d7e9b67759e
Jinser Kafka <aimer@purejs.icu>
2026-08-04 02:35:43 +0800

    use git with perlSupport

--- troisModules/home/git.nix
+++ troisModules/home/git.nix
@@ -12,9 +12,6 @@
       in
       {
         enable = true;
-        package = pkgs.git.override {
-          perlSupport = false;
-        };
         lfs.enable = true;
         settings = {
           user.name = "Jinser Kafka";
