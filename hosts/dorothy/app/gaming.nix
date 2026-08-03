d8ba4ca04388ef9f61e780a5a7cd28a010b14b10
Jinser Kafka <aimer@purejs.icu>
2026-08-04 02:28:27 +0800

    prismlauncher

--- hosts/dorothy/app/gaming.nix
+++ hosts/dorothy/app/gaming.nix
@@ -7,32 +7,54 @@
 }:
 
 let
-  enable = false;
-
   inherit (flake.config.symbols.people) myself;
-  cfg = config.programs.steam;
+  inherit (flake.config.lib) mkHM;
+
+  cfgSteam = config.programs.steam;
+  cfgPrism = true;
 in
-{
-  imports = [ flake.config.modules.nixos.misc ];
+lib.mkMerge [
+  {
+    imports = [ flake.config.modules.nixos.misc ];
+  }
 
-  programs.steam = {
-    inherit enable;
-    package = pkgs.steam.override {
-      extraEnv.DISPLAY = ":0";
+  (lib.mkIf cfgPrism (
+    (mkHM (
+      { pkgs, ... }: {
+        home.packages = [
+          (pkgs.prismlauncher.override {
+            jdks = [ pkgs.graalvmPackages.graalvm-ce ];
+          })
+        ];
+      }
+    ))
+    // {
+      preservation.preserveAt."/persist" = {
+        users.${myself}.directories = [ ".local/share/PrismLauncher" ];
+      };
+    }
+  ))
+
+  {
+    programs.steam = {
+      enable = false;
+      package = pkgs.steam.override {
+        extraEnv.DISPLAY = ":0";
+      };
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = false;
+      dedicatedServer.openFirewall = false;
     };
-    gamescopeSession.enable = true;
-    remotePlay.openFirewall = false;
-    dedicatedServer.openFirewall = false;
-  };
 
-  preservation.preserveAt."/persist" = lib.mkIf cfg.enable {
-    users.${myself}.directories = [ ".local/share/Steam" ];
-  };
+    preservation.preserveAt."/persist" = lib.mkIf cfgSteam.enable {
+      users.${myself}.directories = [ ".local/share/Steam" ];
+    };
 
-  nixpkgs.superConfig.allowUnfreeList = lib.mkIf cfg.enable [
-    "steam"
-    "steam-unwrapped"
-    "steam-original"
-    "steam-run"
-  ];
-}
+    nixpkgs.superConfig.allowUnfreeList = lib.mkIf cfgSteam.enable [
+      "steam"
+      "steam-unwrapped"
+      "steam-original"
+      "steam-run"
+    ];
+  }
+]
